aa9acdcb4433e57f9e168fad0d359d4138a8e818
Roman <roman.3.ry@gmail.com>
2025-05-14 20:03:15 +0300

    Creating initial templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,10 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Home")
+rooms = [
+    {"id": 1, "name": "Let's learn Python"},
+    {"id": 2, "name": "Let's learn Design"},
+    {"id": 3, "name": "Let's learn ML"},
+]
 
-def room(request):
-    return HttpResponse("Room")
+def home(request):
+    context = {"rooms": rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}
+    return render(request, 'base/room.html', context)
